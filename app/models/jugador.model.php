08e025834d024c7d386a8eb5807cf65ad1df4182
Benjamin2710 <132313592+Benjamin2710@users.noreply.github.com>
2025-10-21 21:42:13 -0300

    Update jugador.model.php

--- app/models/jugador.model.php
+++ app/models/jugador.model.php
@@ -10,7 +10,7 @@ class JugadorModel {
         $this->db = $conex->conexion(); // Guardamos la conexión PDO
     }
 
-    // Obtener todos los jugadores junto con el nombre del equipo
+    // jugdores u qeuipo
     public function getJugadores() {
         $query = $this->db->prepare('
             SELECT jugadores.*, equipos.nombre AS nombre_club 
@@ -21,37 +21,42 @@ class JugadorModel {
         return $query->fetchAll(PDO::FETCH_OBJ);
     }
 
-    // Obtener un jugador por ID
+    // pided por ID juador
     public function getJugador($id) {
         $query = $this->db->prepare('SELECT jugadores.*, equipos.nombre AS nombre_club FROM jugadores INNER JOIN equipos ON jugadores.id_equipo = equipos.id_equipo WHERE id_jugador = ?');
         $query->execute([$id]);
         return $query->fetch(PDO::FETCH_OBJ);
     }
 
-    // Borrar un jugador por ID
-    public function eraseJugador($id) {
-        $query = $this->db->prepare('DELETE FROM jugadores WHERE id_jugador = ?');
-        $query->execute([$id]);
-    }
+   
 
-    // Insertar un nuevo jugador
+    // Inserta nuevo jugador
     public function insertJugador($nombre, $pais, $posicion, $puntaje, $fecha_nacimiento, $id_equipo) {
         $query = $this->db->prepare('INSERT INTO jugadores(nombre, pais, posicion, puntaje, fecha_nacimiento, id_equipo) VALUES (?, ?, ?, ?, ?, ?)');
         $query->execute([$nombre, $pais, $posicion, $puntaje, $fecha_nacimiento, $id_equipo]);
         return $this->db->lastInsertId();
     }
 
-    // Actualizar un jugador existente (necesita recibir el ID)
+    // Actualiza existente (ID)
     public function updateJugador($id, $nombre, $pais, $posicion, $puntaje, $fecha_nacimiento, $id_equipo) {
         $query = $this->db->prepare('UPDATE jugadores SET nombre = ?, pais = ?, posicion = ?, puntaje = ?, fecha_nacimiento = ?, id_equipo = ? WHERE id_jugador = ?');
         $query->execute([$nombre, $pais, $posicion, $puntaje, $fecha_nacimiento, $id_equipo, $id]);
 }
+     // Borra por ID
+    public function eraseJugador($id) {
+        $query = $this->db->prepare('DELETE FROM jugadores WHERE id_jugador = ?');
+        $query->execute([$id]);
+    }
 
 
     // Obtener jugadores de un equipo específico
     public function getJugadoresConNombreDeEquipoByEquipoId($id_equipo) {
+           //filtrando jugadores por el equipo
+        
         $query = $this->db->prepare('SELECT jugadores.*, equipos.nombre AS nombre_club FROM jugadores INNER JOIN equipos ON jugadores.id_equipo = equipos.id_equipo WHERE jugadores.id_equipo = ?');
         $query->execute([$id_equipo]);
         return $query->fetchAll(PDO::FETCH_OBJ);
+
+        
     }
 }
